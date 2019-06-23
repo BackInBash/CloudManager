7baf4206a1c85ca561a403bb36d0e443885da675
markus <>
2019-06-23 15:58:20 +0200

    Prepare for Hetzner Status Feature

--- src/main/java/de/markus/cloudmanager/shared/cloud/hetzner/status.java
+++ src/main/java/de/markus/cloudmanager/shared/cloud/hetzner/status.java
@@ -10,7 +10,6 @@ import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
-import de.markus.cloudmanager.shared.models.HetznerStatus;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -26,15 +25,28 @@ public class status {
     public status() {
 
     }
-    
-    public static final String HETZNER_STATUS = "https://www.hetzner-status.de/en.atom";
 
-    public List<HetznerStatus> parse() throws XMLStreamException, IOException, IllegalArgumentException, FeedException {
+    private static final String HETZNER_STATUS = "https://www.hetzner-status.de/en.atom";
+    
+    private long CloudNodeID(String title){
+        String tmp = title.replaceAll("([a-z]|\\(.*\\))", "");
+        return Long.parseLong(tmp);
+    }
+
+    public List<String> parse() throws XMLStreamException, IOException, IllegalArgumentException, FeedException {
         SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(HETZNER_STATUS)));
-        List<HetznerStatus> stats = new ArrayList<HetznerStatus>();
+        List<String> stats = new ArrayList<String>();
         for (SyndEntry sy : feed.getEntries()) {
             if (!sy.getDescription().getValue().contains("<strong>End</strong>")) {
                 // Save Results to List
+                if (sy.getTitle().contains("cloud node")) {
+                    if (!sy.getDescription().getValue().contains("All systems are reachable again")) {
+                        if (!sy.getDescription().getValue().contains("The server is back online")) {
+                            System.out.println(sy);
+                            CloudNodeID(sy.getTitle());
+                        }
+                    }
+                }
             }
         }
         return stats;
